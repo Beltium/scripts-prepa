fa0fc7d2fbf5c8637444d636de5f5c486f569866
Beltium <mathieuhugo2008@gmail.com>
2026-09-06 14:19:57 +0200

    script2.py

---
+++ Physique/TP 1/script2.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Paramètres
+l = 1
+u_l = 0.005
+
+g = 9.81
+u_g = 0.01
+
+n = 3 # Nombre chiffres significatifs
+N = 10000 # Nombre de simulations
+
+# Calcul aléatoire des valeurs
+valeurs_l = np.random.uniform(l-u_l, l+u_l, N)
+valeurs_g = np.random.uniform(g-u_g, g+u_g, N)
+
+# Calcul de la période
+T = 2 * np.pi * np.sqrt(valeurs_l/valeurs_g)
+
+# Statistiques
+moyenne = np.mean(T)
+incertitudetype = np.std(T, ddof=1)
+
+# Affichage
+print(f"Nombre de simulations : {N}")
+print(f"Valeur moyenne : {moyenne:.{n-1}e}")
+print(f"Incertitude-type composée : {incertitudetype:.{n-1}e}")
+
+# Histogramme
+plt.hist(T, bins = 100)
+plt.axvline(moyenne, color="red", label="Moyenne")
+plt.xlabel("Période T (s)")
+plt.ylabel("Nombre de simulations")
+plt.title("Simulation aléatoire de la période du pendule")
+plt.text(
+    0.05, 0.95, # Position dans le cadre (entre 0 et 1)
+    f"Moyenne = {moyenne:.{n-1}e}\n"
+    f"Incertitude-type = {incertitudetype:.{n-1}e}",
+    transform=plt.gca().transAxes, # Transformer les coordonnées en coordonnées entre 0 et 1
+    verticalalignment="top"
+)
+plt.legend()
+plt.show()
